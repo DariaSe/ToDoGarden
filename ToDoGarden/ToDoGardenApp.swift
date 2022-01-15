8c38d1ecbe89cd444271876dab7260bb222ed0a3
DariaSe <daria.samosledova@icloud.com>
2022-01-15 15:14:15 +0300

    Initial Commit

---
+++ ToDoGarden/ToDoGardenApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoGardenApp.swift
+//  ToDoGarden
+//
+//  Created by Дарья Селезнёва on 15.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct ToDoGardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
